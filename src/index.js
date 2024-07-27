9d88c88c8b049ebdca24e7847f8769addaf05381
Saikat Shasmal <s.shasmal1980@gmail.com>
2024-07-27 21:45:29 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectdb from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+connectdb();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+    app.on("error", () => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})();
+
+*/
